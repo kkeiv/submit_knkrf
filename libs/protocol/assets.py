a7aeacf496a11f3582871c9a2005f2745cdf518a
Evgeny Krasnopevtsev <evgenykrasnopevtsev@gmail.com>
2025-01-24 02:07:30 +0300

    add changes for response

--- libs/protocol/assets.py
+++ libs/protocol/assets.py
@@ -27,6 +27,7 @@ class Params(Enum):
     par_11 = {"num": 11, "pname": "serial", "type": "string", "len": 10}
     par_12 = {"num": 12, "pname": "device_type", "type": "dec", "len": 2}
     par_13 = {"num": 13, "pname": "device_version", "type": "dec", "len": -1}
+    par_15 = {"num": 15, "pname": "lcdLight", "type": "dec", "len": 1}
     par_20 = {"num": 20, "pname": "par1", "type": "float", "len": -1}
     par_21 = {"num": 21, "pname": "par2", "type": "float", "len": -1}
     par_22 = {"num": 22, "pname": "par3", "type": "float", "len": -1}
